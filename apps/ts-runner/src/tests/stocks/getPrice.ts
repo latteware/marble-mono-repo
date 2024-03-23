b7efff6b3515afb2bd5eb455e902c51c6e6e2a47
Siedrix Zavala <Siedrix@gmail.com>
2024-03-22 21:48:07 -0600

    Add errores to posible log output and update the template

--- apps/ts-runner/src/tests/stocks/getPrice.ts
+++ apps/ts-runner/src/tests/stocks/getPrice.ts
@@ -13,17 +13,33 @@ const tape = new RecordTape({
 })
 
 tape.loadSync()
+console.log('cache ->', tape.compileCache().fetchDelta)
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
 getPrice.setBoundariesData(tape.compileCache())
 
 describe.only('stocks:getPrice tape', function () {
   tape.getLog().forEach((logRecord, i) => {
-    it(`Tape #${i}: ${JSON.stringify(logRecord.input)}`, async function () {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-      const res = await getPrice.run(logRecord.input as unknown as TastArgv)
+    let label = `Tape #${i}: ${JSON.stringify(logRecord.input)} with success result`
 
-      expect(res).to.be.deep.equal(logRecord.output)
+    if (logRecord.error !== undefined) {
+      label = `Tape #${i}: ${JSON.stringify(logRecord.input)} with error`
+    }
+    it(label, async function () {
+      let res, error: Error | undefined
+      try {
+        res = await getPrice.run(logRecord.input as unknown as TastArgv)
+      } catch (e) {
+        error = e
+      }
+
+      if (error !== undefined) {
+        expect(res).to.be.equal(undefined)
+        expect(error.message).to.be.equal(logRecord.error)
+      } else {
+        expect(res).to.be.deep.equal(logRecord.output)
+        expect(error).to.be.equal(undefined)
+      }
     })
   })
 })

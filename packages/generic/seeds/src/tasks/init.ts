5459e407086299a821b2d3afd82ac048f3cb360f
Siedrix Zavala <Siedrix@gmail.com>
2024-03-23 19:35:28 -0600

    Updated seeds init to have task and runners

--- packages/generic/seeds/src/tasks/init.ts
+++ packages/generic/seeds/src/tasks/init.ts
@@ -2,20 +2,24 @@ import { Task } from '@marble-seeds/task'
 import path from 'path'
 import fs from 'fs/promises'
 
+import { type SeedsConf } from './types'
+
 // Remove this comment once you add the params options that that the task allow
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface TastArgv {}
 
 export const init = new Task(async function (argv: TastArgv) {
   const seedsPath = path.join(process.cwd(), 'seeds.json')
-  const config = {
+  const config: SeedsConf = {
     paths: {
       logs: 'logs/',
       tasks: 'src/tasks/',
       runners: 'src/runners/',
       fixtures: 'src/tests/fixtures',
       tests: 'src/tests/'
-    }
+    },
+    tasks: {},
+    runners: {}
   }
 
   await fs.writeFile(seedsPath, JSON.stringify(config, null, 2))
